f91b79dec054052bd8bd1f6f41b8d01234542df2
Jonghyeon Ko <flowithusdev@gmail.com>
2021-12-02 09:41:26 +0900

    ✨ feat: base 컴포넌트들 추가

---
+++ src/components/base/Avatar/AvatarGroup.tsx
@@ -0,0 +1,43 @@
+import React, { ReactElement, ReactNode } from "react";
+import { AvatarShape } from "./types";
+
+interface Props {
+  children: ReactNode;
+  shape?: AvatarShape;
+  size?: number;
+}
+
+const AvatarGroup = ({
+  children,
+  shape = "circle",
+  size = 70,
+  ...props
+}: Props) => {
+  const avatars = React.Children.toArray(children)
+    .filter((element) => {
+      if (React.isValidElement(element) && element.props.__TYPE === "Avatar") {
+        return true;
+      }
+
+      console.warn("Only accepts Avatar as it's children.");
+      return false;
+    })
+    .map((avatar, index, avatars) => {
+      const item = avatar as ReactElement;
+      return React.cloneElement(item, {
+        ...item.props,
+        size,
+        shape,
+
+        style: {
+          ...item.props.style,
+          marginLeft: -size / 5,
+          zIndex: avatars.length - index,
+        },
+      });
+    });
+
+  return <div style={{ paddingLeft: size / 5 }}>{avatars}</div>;
+};
+
+export default AvatarGroup;
---
+++ src/components/base/Avatar/index.tsx
@@ -0,0 +1,85 @@
+import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+import ImageComponent from "../Image";
+import AvatarGroup from "./AvatarGroup";
+import { AvatarShape } from "./types";
+
+interface Props {
+  lazy?: boolean;
+  threshold?: number;
+  src: string;
+  size?: number;
+  shape?: AvatarShape;
+  placeholder?: string;
+  alt?: string;
+  mode?: "cover" | "fill" | "contain";
+  __TYPE: "Avatar";
+}
+
+const Avatar = ({
+  lazy,
+  threshold,
+  src,
+  size = 70,
+  shape = "round",
+  placeholder,
+  alt,
+  mode = "cover",
+  __TYPE = "Avatar",
+  ...props
+}: Props) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = src;
+    image.onload = () => setLoaded(true);
+  }, [src]);
+
+  return (
+    <AvatarWrapper shape={shape} {...props}>
+      <ImageComponent
+        block
+        lazy={lazy}
+        threshold={threshold}
+        width={size}
+        height={size}
+        src={src}
+        placeholder={placeholder}
+        alt={alt}
+        mode={mode}
+        style={{ opacity: loaded ? 1 : 0 }}
+      />
+    </AvatarWrapper>
+  );
+};
+
+interface AvatarWrapperProps {
+  shape: AvatarShape;
+}
+
+const ShapeToCssValue = {
+  circle: "50%",
+  round: "4px",
+  square: "0px",
+};
+
+const AvatarWrapper = styled.div<AvatarWrapperProps>`
+  position: relative;
+  display: inline-block;
+  border: 1px solid #dadada;
+  border-radius: ${({ shape }) => ShapeToCssValue[shape]};
+  background-color: #eee;
+  overflow: hidden;
+  > img {
+    transition: opacity 0.2s ease;
+  }
+`;
+
+Avatar.defaultProps = {
+  __TYPE: "Avatar",
+};
+
+Avatar.Group = AvatarGroup;
+
+export default Avatar;
