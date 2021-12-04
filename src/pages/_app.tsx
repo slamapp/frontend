20852ecb714d1ce9caa6073c758da056e1b5b1f2
Jonghyeon Ko <flowithusdev@gmail.com>
2021-12-04 17:13:31 +0900

    💄 style: nomalize.css cdn _app에 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,22 @@ import Providers from "@contexts/Providers";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Providers>
-      <DefaultLayout>
-        <Component {...pageProps} />
-      </DefaultLayout>
-    </Providers>
+    <>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
+      <Providers>
+        <DefaultLayout>
+          <Component {...pageProps} />
+        </DefaultLayout>
+      </Providers>
+    </>
   );
 }
 
