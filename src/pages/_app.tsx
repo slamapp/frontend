58fbf61157c15157683988a998baf21a03cf6dd7
Jonghyeon Ko <flowithusdev@gmail.com>
2021-12-04 12:54:01 +0900

    ✨ feat: Providers와 DefaultLayout을 전역 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from "next/app";
+import { DefaultLayout } from "@components/layout";
+import Providers from "@contexts/Providers";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Providers>
+      <DefaultLayout>
+        <Component {...pageProps} />
+      </DefaultLayout>
+    </Providers>
+  );
 }
 
 export default MyApp;
