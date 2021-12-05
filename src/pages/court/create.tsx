b408513d2419649cccbdb5962d218016ac9118d8
limkhl <limkh.dev@gmail.com>
2021-12-05 16:14:05 +0900

    ✨ feat: 농구장 이름, 골대 개수 value 받기 위한 useForm 커스텀 훅 적용

--- src/pages/court/create.tsx
+++ src/pages/court/create.tsx
@@ -4,31 +4,90 @@ import Text from "@components/base/Text";
 import Button from "@components/Button";
 import type { NextPage } from "next";
 import Head from "next/head";
+import useForm from "../../hooks/useForm";
+
+interface Values {
+  longitude: number;
+  latitude: number;
+  image: string | null;
+  texture: string | null;
+  basketCount: string; // TODO: 백엔드에 string으로 수정 요청
+  courtName: string;
+}
 
 const createCourt: NextPage = () => {
+  const { values, errors, isLoading, handleChange, handleSubmit } =
+    useForm<Values>({
+      initialValues: {
+        longitude: 0,
+        latitude: 0,
+        image: null,
+        texture: null,
+        basketCount: "1",
+        courtName: "",
+      },
+      onSubmit: (values) => {
+        alert(JSON.stringify(values));
+      },
+      validate: ({
+        // longitude,
+        // latitude,
+        // image,
+        // texture,
+        basketCount,
+        courtName,
+      }) => {
+        const errors: Partial<Values> = {};
+
+        if (!courtName) {
+          errors.courtName = "농구장 이름을 입력해주세요.";
+        }
+        if (!basketCount) {
+          errors.basketCount = "골대 개수를 입력해주세요.";
+        }
+
+        return errors;
+      },
+    });
+
   return (
     <div>
       <Head>
         <title>새 농구장 추가</title>
       </Head>
-      <form>
+
+      <form onSubmit={handleSubmit}>
         <Spacer size={24} type="vertical">
-          <Input label="농구장 이름" type="text" name="courtName" block />
-          <Text>위치</Text>
-          <div
-            style={{ width: "100%", height: "200px", background: "lightblue" }}
-          >
-            지도 맵 영역
+          <div>
+            <Input
+              label="농구장 이름"
+              type="text"
+              name="courtName"
+              onChange={handleChange}
+              value={values.courtName}
+            />
+            <span>{errors.courtName}</span>
           </div>
-          <Input
-            label="골대 개수"
-            type="number"
-            name="courtCount"
-            min="0"
-            max="100"
-            required
-          />
-          <Button type="submit">제출하기</Button>
+          <div>
+            <Text>위치</Text>
+            <Button type="button" onClick={() => alert("//TODO: 모달 띄우기")}>
+              지도 맵 영역
+            </Button>
+          </div>
+          <div>
+            <Input
+              label="골대 개수"
+              type="number"
+              name="courtCount"
+              min="0"
+              max="100"
+              onChange={handleChange}
+              value={values.basketCount}
+              required
+            />
+            <span>{errors.basketCount}</span>
+          </div>
+          <Button type="submit">{isLoading ? "Loading..." : "제출하기"}</Button>
         </Spacer>
       </form>
     </div>
