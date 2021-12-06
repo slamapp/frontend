d70573a3b08161ac1f83873a24f89a4593f5ca34
jeonghee <grighth12@gmail.com>
2021-12-06 17:05:08 +0900

    ♻️ refactor: kakao map 공통 컴포넌트로 사용할 수 있도록 리패토링

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from "next";
-import { useState } from "react";
+import { useState, useCallback, useEffect } from "react";
 import KakaoMap from "@components/KakaoMap";
+import { getCurrentLocation, DEFAULT_POSITION } from "@utils/geolocation";
+import GeneralMarker from "@components/KakaoMapMarker/GeneralMarker";
 import { Coord } from "../../types/map";
 
 declare global {
@@ -25,6 +27,9 @@ const Map: NextPage = () => {
   const [visible, setVisible] = useState<boolean>(false);
   const [selectedCourt, setSelectedCourt] = useState<any>();
   const [isAddressLoading, setIsAddressLoading] = useState<boolean>(false);
+  const [level, setLevel] = useState<number>(3);
+  const [center, setCenter] = useState<Coord>(DEFAULT_POSITION);
+  const [position, setPosition] = useState<Coord>();
 
   const searchAddrFromCoords = ([latitude, longitude]: Coord) => {
     const geocoder = new kakao.maps.services.Geocoder();
@@ -67,9 +72,65 @@ const Map: NextPage = () => {
     setSelectedCourt(court);
   };
 
+  const handleChangeCenterPosition = useCallback(() => {
+    getCurrentLocation(([latitude, longitude]) => {
+      setCenter([latitude, longitude]);
+    });
+  }, []);
+
+  const handleGetMapCenter = useCallback((map: kakao.maps.Map) => {
+    const bounds = map.getBounds();
+    const swLatlng = bounds.getSouthWest();
+    // cons neLatlng = bounds.getNorthEast();
+
+    console.log([swLatlng.getLat(), swLatlng.getLng()]);
+  }, []);
+
+  const onClick = (
+    _: kakao.maps.Map,
+    mouseEvent: kakao.maps.event.MouseEvent
+  ) => {
+    // 클릭한 위도, 경도 정보를 가져옵니다
+    const latlng = mouseEvent.latLng;
+
+    if (latlng) {
+      console.log(
+        `클릭한 위치의 위도는 ${latlng.getLat()} 이고, 경도는 ${latlng.getLng()} 입니다`
+      );
+      setPosition([latlng.getLat(), latlng.getLng()]);
+    }
+  };
+
+  const handleZoomIn = useCallback(() => {
+    setLevel((level) => level - 1);
+  }, []);
+
+  const handleZoomOut = useCallback(() => {
+    setLevel((level) => level + 1);
+  }, []);
+
+  useEffect(() => {
+    handleChangeCenterPosition();
+  }, [handleChangeCenterPosition]);
+
   return (
     <>
-      <KakaoMap onMarkerClick={handleMarkerClick} />
+      <button type="button" onClick={handleChangeCenterPosition}>
+        현재 내 위치 받아오기
+      </button>
+      <button type="button" onClick={handleZoomIn}>
+        확대(줌 레벨 -1)
+      </button>
+      <button type="button" onClick={handleZoomOut}>
+        축소(줌 레벨 +1)
+      </button>
+      <KakaoMap
+        level={level}
+        center={center}
+        onClick={onClick}
+        onDragEnd={handleGetMapCenter}
+        position={position}
+      />
 
       {isAddressLoading ? <div>로딩중...</div> : null}
       {!isAddressLoading && visible ? (
