edec9f0cf918378d9778a8c9b4e01721e4b9b903
limkhl <limkh.dev@gmail.com>
2021-12-08 03:08:32 +0900

    🚧 chore: 새 농구장 추가 페이지 진입용 임시 링크 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Button from "@components/Button";
 import { useNavigationContext } from "@contexts/NavigationProvider";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   const { useMountPage } = useNavigationContext();
@@ -15,6 +16,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>홈 페이지</h1>
+      <Link href="/court/create">새 농구장 추가</Link>
       <Button>Click Me!</Button>
     </div>
   );
