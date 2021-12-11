0573ff12c6b46610952ece290858f961b663268e
Jonghyeon Ko <flowithusdev@gmail.com>
2021-12-11 20:25:41 +0900

    ✨ feat: redirect페이지에서 토큰 받아오기

--- src/pages/login/redirect/index.tsx
+++ src/pages/login/redirect/index.tsx
@@ -1,13 +1,36 @@
+import React, { useEffect } from "react";
 import { Spinner } from "@components/base";
 import styled from "@emotion/styled";
 import { useRouter } from "next/router";
-import React from "react";
+import { serviceApi } from "service";
 
 const RedirectPage = () => {
-  const { query } = useRouter();
+  const router = useRouter();
+  const { code: kakaoAuthCode } = router.query;
 
-  console.log(query);
-  console.log(query.code);
+  const getServiceToken = async (kakaoAuthCode: string) => {
+    try {
+      const {
+        data: { serviceToken },
+      } = await serviceApi.getServiceToken({ kakaoAuthCode });
+
+      // TODO: 서비스 토큰 로컬스토리지에 저장하기
+      console.log(serviceToken);
+
+      // 로그인 완료
+      // router.replace("/");
+    } catch (e) {
+      router.replace("/login");
+    }
+  };
+
+  useEffect(() => {
+    if (kakaoAuthCode) {
+      console.log("hi");
+
+      getServiceToken(router.query.code as string);
+    }
+  }, [kakaoAuthCode]);
 
   return (
     <PageContainer>
