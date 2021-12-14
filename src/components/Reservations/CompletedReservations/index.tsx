e8f49317a47ace00eb39bd71e66ebb534d499c1a
Parkserim <tpfla6661@naver.com>
2021-12-14 14:55:56 +0900

    ✨ feat: 지난 예약 컴포넌트 구현

---
+++ src/components/Reservations/CompletedReservations/index.tsx
@@ -0,0 +1,56 @@
+import { NextPage } from "next";
+import { type } from "os";
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import Link from "next/link";
+import Participants from "../Participants";
+
+interface ReserveList {
+  reservationId: number;
+  courtName: string;
+  playTime: string;
+}
+
+type ReserveLists = ReserveList[];
+
+const CompletedReservations: NextPage = () => {
+  const DummyReserves = [
+    {
+      reservationId: 1,
+      courtName: "용왕산 근린 공원 농구장",
+      playTime: "11월 20일 토요일 11시 - 12시",
+    },
+    {
+      reservationId: 2,
+      courtName: "백두산 근린 공원 농구장",
+      playTime: "12월 20일 토요일 11시 - 12시",
+    },
+    {
+      reservationId: 3,
+      courtName: "북한산 근린 공원 농구장",
+      playTime: "1월 20일 토요일 11시 - 12시",
+    },
+  ];
+
+  return (
+    <>
+      {DummyReserves.map(({ reservationId, courtName, playTime }) => (
+        <BorderDiv key={reservationId}>
+          <p>{courtName}</p>
+          <p>{playTime}</p>
+          <Link href="/">
+            <button>예약 보기</button>
+          </Link>
+          <Participants />
+        </BorderDiv>
+      ))}
+    </>
+  );
+};
+
+const BorderDiv = styled.div`
+  border: 1px solid;
+  margin-top: 30px;
+`;
+
+export default CompletedReservations;
