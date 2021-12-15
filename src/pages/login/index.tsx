31ea6f47282d9d79a80e0903314f2e8c79844b58
Jonghyeon Ko <flowithusdev@gmail.com>
2021-12-16 05:48:05 +0900

    💄 style: 로그인 페이지 스타일링

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,63 +1,59 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { KakaoLogin } from "@components/domain";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import UtilRoute from "UtilRoute";
-import { useEffect } from "react";
 import { useNavigationContext } from "@contexts/hooks";
+import { useRouter } from "next/router";
+import { BottomFixedButton, Logo } from "@components/domain";
+import styled from "@emotion/styled";
+import { Header, Spacer } from "@components/base";
 
 const Login: NextPage = UtilRoute("prevented", () => {
-  const { useMountPage, setCustomButtonEvent, clearNavigationEvent } =
-    useNavigationContext();
-  useMountPage((page) => page.LOGIN);
   const router = useRouter();
+  const { useMountPage } = useNavigationContext();
+  useMountPage((page) => page.LOGIN);
 
   const endpoint = process.env.NEXT_PUBLIC_SERVICE_API_END_POINT as string;
+  const kakaoUrl = `${endpoint}/oauth2/authorization/kakao?redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}`;
 
-  const kakaoUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&response_type=code`;
-  const handleClick = () => router.replace(kakaoUrl);
-
-  const jellyKakaoUrl = `${endpoint}/oauth2/authorization/kakao?redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}`;
-
-  useEffect(() => {
-    setCustomButtonEvent("취소", () => alert("페이지에서 바인딩"));
-    return clearNavigationEvent;
-  }, []);
+  const handleClickLogin = () => {
+    router.replace(kakaoUrl);
+  };
 
   return (
-    <div>
+    <PageContainer>
       <Head>
         <title>로그인 | Slam - 우리 주변 농구장을 빠르게</title>
         <meta name="description" content="혼자서도 농구를 더 빠르게" />
       </Head>
 
-      <Link href={jellyKakaoUrl}>
-        <a>젤리의 카카오 계정 로그인</a>
-      </Link>
+      <Spacer gap={6} type="vertical" style={{ alignItems: "center" }}>
+        <Logo width={160} />
+        <DescriptionText>같이 농구할 사람이 없다고?</DescriptionText>
+      </Spacer>
 
-      <button onClick={handleClick}>카카오 계정 로그인</button>
-
-      <Link href={kakaoUrl}>
-        <a>카카오 계정 로그인</a>
-      </Link>
-
-      <KakaoLogin
-        token={process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY as string}
-        onSuccess={(result) => {
-          alert(`로그인에 성공하셨습니다. ${JSON.stringify(result)}`);
-          console.log(result);
-        }}
-        onFail={(result) => {
-          alert(`로그인에 실패하셨습니다. ${JSON.stringify(result)}`);
-          console.log(result);
-        }}
-        onLogout={() => {
-          alert(`로그아웃 하셨습니다.`);
-        }}
-      />
-    </div>
+      <KaKaoLoginButton onClick={handleClickLogin}>
+        카카오 로그인
+      </KaKaoLoginButton>
+    </PageContainer>
   );
 });
 
 export default Login;
+
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90%;
+`;
+
+const DescriptionText = styled.span`
+  font-size: 16;
+  font-weight: 900;
+`;
+
+const KaKaoLoginButton = styled(BottomFixedButton)`
+  background-color: ${({ theme }) => theme.colors.kakao.yellow.strong};
+  color: ${({ theme }) => theme.colors.kakao.brown.strong};
+`;
