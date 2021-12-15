547ab9deae5fcbd2e8b541f1d010797ca9a28274
limkhl <limkh.dev@gmail.com>
2021-12-15 19:01:09 +0900

    🚚 move: Radio Input 컴포넌트 base 폴더 안으로 이동

--- src/components/domain/UserInfoPicker/ProficiencyPicker.tsx
+++ src/components/domain/UserInfoPicker/ProficiencyPicker.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent } from "react";
-import Radio from "@components/domain/RadioInput";
+import Radio from "@components/base/RadioInput";
 import type { ProficiencyKeyUnion, ProficiencyValueUnion } from "./types";
 
 const proficiencyItems: {
