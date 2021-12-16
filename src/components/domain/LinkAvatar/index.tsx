da91db03df62689133628dd62b3c3b9415f27d43
Jonghyeon Ko <flowithusdev@gmail.com>
2021-12-17 00:06:49 +0900

    :recycle: refactor: LinkAvatar 리팩토링

--- src/components/domain/LinkAvatar/index.tsx
+++ src/components/domain/LinkAvatar/index.tsx
@@ -2,17 +2,37 @@ import { Avatar } from "@components/base";
 import Link from "next/link";
 import React from "react";
 
+type Size = "large" | "middle" | "small";
+
 interface Props {
   userId: string | number;
   imageUrl: string;
+  size?: Size;
 }
 
-const LinkAvatar = ({ userId, imageUrl }: Props) => {
+const LinkAvatar = ({ userId, imageUrl, size = "middle" }: Props) => {
   return (
     <Link href={`/user/${userId || 1}`} passHref>
-      <Avatar size={32} src={imageUrl} />
+      <Avatar size={getSize(size)} src={imageUrl} shape="circle" />
     </Link>
   );
 };
 
 export default LinkAvatar;
+
+const getSize = (size: Size) => {
+  switch (size) {
+    case "large":
+      return 50;
+      break;
+    case "middle":
+      return 32;
+      break;
+    case "small":
+      return 26;
+      break;
+    default:
+      return 32;
+      break;
+  }
+};
