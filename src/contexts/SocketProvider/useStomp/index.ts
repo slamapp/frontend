570a06a076555599649efd313c98aa28232eb400
Jonghyeon Ko <flowithusdev@gmail.com>
2021-12-16 22:04:45 +0900

    ✨ feat: util함수 useStomp로 연결

--- src/contexts/SocketProvider/useStomp/index.ts
+++ src/contexts/SocketProvider/useStomp/index.ts
@@ -1,14 +1,16 @@
+import { Notification } from "@contexts/AuthProvider/types";
 import { useAuthContext } from "@contexts/hooks";
 import { CompatClient } from "@stomp/stompjs";
 import { useCallback, useEffect, useState } from "react";
 import { socketApi } from "service";
 import { SendAuth, UseStomp } from "./type";
+import { subscribe } from "./utils";
 
 const useStomp: UseStomp = (token: string) => {
   const [compatClient, setCompatClient] = useState<CompatClient | null>(null);
   const [isConnected, setIsConnected] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const { authProps } = useAuthContext();
+  const { authProps, pushNotification } = useAuthContext();
   const {
     currentUser: { userId },
   } = authProps;
@@ -40,12 +42,14 @@ const useStomp: UseStomp = (token: string) => {
         () => {
           setIsConnected(true);
           setIsLoading(false);
-          compatClient.subscribe("/topic/teston", ({ body }) => {
-            console.log("/topic/teston:::::", body);
-          });
 
-          compatClient.subscribe(`/topic/${userId}`, ({ body }) => {
-            console.log(`/topic/${userId}`, body);
+          subscribe(compatClient, `/topic/${userId}`, (body) => {
+            console.log(body);
+          });
+          subscribe(compatClient, `/user/${userId}/notification`, (body) => {
+            console.log(body);
+
+            pushNotification(body as Notification);
           });
         },
         handleError
@@ -54,6 +58,8 @@ const useStomp: UseStomp = (token: string) => {
   }, [compatClient]);
 
   const sendAuth: SendAuth = (destination, body) => {
+    console.log(destination, body);
+
     const bodyStringified = JSON.stringify(body);
     if (compatClient && token)
       compatClient.send(destination, { token }, bodyStringified);
