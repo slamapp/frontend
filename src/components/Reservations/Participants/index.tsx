47e4e8b61d411f822181c138ecec0998ac534474
Parkserim <tpfla6661@naver.com>
2021-12-16 23:24:27 +0900

    💄 style: Participants 컴포넌트 스타일링

--- src/components/Reservations/Participants/index.tsx
+++ src/components/Reservations/Participants/index.tsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { Avatar, Button, IconButton, Spacer, Text } from "@components/base";
 
 interface PlayerList {
+  followId: number;
   userId: number;
-  userImageUrl: string;
-  userName: string;
-  isFollow: boolean;
+  nickname: string;
+  isFollowed: boolean;
+  profileImage: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 type PlayerLists = PlayerList[];
@@ -13,66 +18,96 @@ const Participants = () => {
   const [visible, setVisible] = useState(false);
   const [state, setState] = useState([
     {
-      userId: 1,
-      userImageUrl: "img1",
-      userName: "참가자1",
-      isFollow: true,
-    },
-    {
+      followId: 3,
       userId: 2,
-      userImageUrl: "img2",
-      userName: "참가자2",
-      isFollow: false,
-    },
-    {
-      userId: 3,
-      userImageUrl: "img3",
-      userName: "참가자3",
-      isFollow: true,
+      nickname: "hey",
+      isFollowed: true,
+      profileImage: "https://picsum.photos/250/250",
+      createdAt: "2021-01-01T12:20:10",
+      updatedAt: "2021-01-01T12:20:10",
     },
     {
       userId: 4,
-      userImageUrl: "img4",
-      userName: "참가자4",
-      isFollow: false,
+      nickname: "florar",
+      isFollowed: false,
+      profileImage: "https://a3.com",
+      createdAt: "2021-01-01T12:20:10",
+      updatedAt: "2021-01-01T12:20:10",
+    },
+    {
+      userId: 5,
+      nickname: "jelli",
+      isFollowed: false,
+      profileImage: "https://a3.com",
+      createdAt: "2021-01-01T12:20:10",
+      updatedAt: "2021-01-01T12:20:10",
     },
   ]);
 
   const toggleIsFollow = (userId: number) => {
     const newState = state.map((_state) =>
       _state.userId === userId
-        ? { ..._state, isFollow: !_state.isFollow }
+        ? { ..._state, isFollowed: !_state.isFollowed }
         : _state
     );
     setState(newState);
   };
 
   return (
-    <>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "함께하는 사람 숨기기" : "함께하는 사람 보기"}
-      </button>
+    <Spacer gap="lg" type="vertical">
+      <div>
+        {visible ? (
+          <IconButton
+            name="users"
+            type="button"
+            onClick={() => setVisible(!visible)}
+          ></IconButton>
+        ) : (
+          <IconButton
+            name="users"
+            type="button"
+            onClick={() => setVisible(!visible)}
+          ></IconButton>
+        )}
+      </div>
       {visible && (
-        <div>
-          {state.map(({ userId, userImageUrl, userName, isFollow }) => (
-            <div key={userId}>
-              <p>
-                {userImageUrl}
-                {userName}
-                <button onClick={() => toggleIsFollow(userId)}>
-                  {isFollow ? "팔로잉" : "팔로우"}
-                </button>
-              </p>
-            </div>
+        <Spacer gap="xs" type="vertical">
+          {state.map(({ userId, profileImage, nickname, isFollowed }) => (
+            <FollowLists className="follow-list" key={userId}>
+              <FollowList>
+                <Avatar shape="circle" size={36} src={profileImage} />
+                <Text size="base" strong>
+                  {nickname}
+                </Text>
+              </FollowList>
+              <div>
+                {isFollowed ? (
+                  <Button onClick={() => toggleIsFollow(userId)}>팔로잉</Button>
+                ) : (
+                  <Button onClick={() => toggleIsFollow(userId)} secondary>
+                    팔로우
+                  </Button>
+                )}
+              </div>
+            </FollowLists>
           ))}
-        </div>
+        </Spacer>
       )}
-    </>
+    </Spacer>
   );
 };
 
 export default Participants;
+
+const FollowLists = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FollowList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 15px;
+`;
