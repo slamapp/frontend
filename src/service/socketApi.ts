3faf9c99e416d9c87ea4bd43dd83213a3ddf0a1e
Jonghyeon Ko <flowithusdev@gmail.com>
2021-12-17 16:43:22 +0900

    🔧 config: 웹소켓 uri 변경

--- src/service/socketApi.ts
+++ src/service/socketApi.ts
@@ -2,8 +2,8 @@ import { Stomp } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
 
 const endPoint = process.env.NEXT_PUBLIC_SERVICE_API_END_POINT as string;
-const subfix = process.env.NEXT_PUBLIC_SERVICE_API_SUB_FIX as string;
-const socketUrl = `${endPoint + subfix}/ws`;
+const subfix = process.env.NEXT_PUBLIC_SERVICE_WS_SUB_FIX as string;
+const socketUrl = `${endPoint + subfix}`;
 
 const socketApi = {
   getWebSocket: () => {
