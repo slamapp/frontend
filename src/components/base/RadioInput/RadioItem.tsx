f2497a334940642bc165b338a6be2d8f6c64c65c
limkhl <limkh.dev@gmail.com>
2021-12-17 17:43:52 +0900

    :recycle: refactor: RadioItem 컴포넌트 Chip 컴포넌트 이용해 리팩토링

--- src/components/base/RadioInput/RadioItem.tsx
+++ src/components/base/RadioInput/RadioItem.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import { css } from "@emotion/react";
-import { Text } from "@components/base";
+import { Chip } from "@components/base";
 
 interface RadioItemProps {
   value: string;
@@ -10,21 +10,19 @@ interface RadioItemProps {
 
 const RadioItem: React.FC<RadioItemProps> = ({ value, text, checked }) => {
   return (
-    <Label>
+    <StyledLabel>
       <StyledRadio
         type="radio"
         value={value}
         checked={checked}
         onChange={() => {}}
       />
-      <Text size="xs" strong>
-        {text}
-      </Text>
-    </Label>
+      <Chip clickable>{text}</Chip>
+    </StyledLabel>
   );
 };
 
-const Label = styled.label`
+const StyledLabel = styled.label`
   margin-right: ${({ theme }) => theme.gaps.xs};
 `;
 
@@ -32,20 +30,6 @@ const StyledRadio = styled.input`
   display: none;
 
   ${({ theme }) => css`
-    & + span {
-      background-color: ${theme.colors.white};
-      color: ${theme.colors.gray700};
-      border-radius: ${theme.borderRadiuses.lg};
-      padding: ${theme.chipPadding};
-      display: inline-block;
-      margin-bottom: ${theme.gaps.xs};
-      cursor: pointer;
-
-      &:hover {
-        background-color: ${theme.colors.gray300};
-      }
-    }
-
     &:checked + span {
       background-color: ${theme.colors.gray900};
       color: ${theme.colors.white};
