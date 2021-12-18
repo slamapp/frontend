cc592137a03189e6016fdac54dbe64e685c7f256
limkhl <limkh.dev@gmail.com>
2021-12-18 19:01:51 +0900

    ✨ feat: 프로필 페이지 api 작업
    Co-authored-by: Jonghyeon <manudeli@users.noreply.github.com>

--- src/service/userApi.ts
+++ src/service/userApi.ts
@@ -2,6 +2,9 @@ import { request, authRequest, authFileRequest } from "./fetcher";
 
 const userAPI = {
   getUserData: () => authRequest.get("/users/me"),
+  getMyProfile: <R>() => authRequest.get<R, R>("/users/myprofile"),
+  getUserProfile: <R>(userId: number) =>
+    authRequest.get<R, R>(`/users/${userId}`),
 };
 
 export default userAPI;
