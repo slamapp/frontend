c5761a5e1521057a6e673d967a9c1f1089c16754
limkhl <limkh.dev@gmail.com>
2021-12-20 13:33:51 +0900

    🐛 fix: getNewCourts api 함수 무한 스크롤로 수정

--- src/service/managementApi.ts
+++ src/service/managementApi.ts
@@ -1,12 +1,29 @@
 import { request, authRequest, authFileRequest } from "./fetcher";
 
 const managementAPI = {
-  getNewCourts: <R>(status: "READY" | "DONE") =>
-    authRequest.get<R, R>("/management/newCourts", {
-      params: {
-        status,
-      },
-    }),
+  getNewCourts: <R>(
+    status: "READY" | "DONE",
+    isFirst: boolean,
+    lastId: number | undefined | null
+  ) => {
+    return lastId
+      ? authRequest.get<R, R>("/management/newCourts", {
+          params: {
+            status,
+            isFirst,
+            lastId,
+            size: 2,
+          },
+        })
+      : authRequest.get<R, R>("/management/newCourts", {
+          params: {
+            status,
+            isFirst,
+            lastId: 0,
+            size: 2,
+          },
+        });
+  },
   acceptNewCourt: (newCourtId: number) =>
     authRequest.patch("/management/newCourts/accept", newCourtId),
   denyNewCourt: (newCourtId: number) =>
