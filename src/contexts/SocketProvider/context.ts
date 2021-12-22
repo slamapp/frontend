3985061fa89f0c8b2f8dbcb061dca659eb773742
Jonghyeon Ko <flowithusdev@gmail.com>
2021-12-23 00:49:06 +0900

    ✨ feat: 확성기 기능 연결

--- src/contexts/SocketProvider/context.ts
+++ src/contexts/SocketProvider/context.ts
@@ -2,15 +2,15 @@ import { createContext } from "react";
 
 export type SendFollow = (body: { receiverId: number }) => void;
 export type SendFollowCancel = (body: { receiverId: number }) => void;
-
+export type SendLoudSpeaker = (body: {
+  courtId: number;
+  startTime: string;
+  reservationId: number;
+}) => void;
 export interface ContextProps {
   sendFollow: SendFollow;
   sendFollowCancel: SendFollowCancel;
+  sendLoudSpeaker: SendLoudSpeaker;
 }
 
-const initialContext = {
-  sendFollow: () => {},
-  sendFollowCancel: () => {},
-};
-
-export const Context = createContext<ContextProps>(initialContext);
+export const Context = createContext<ContextProps>({} as ContextProps);
