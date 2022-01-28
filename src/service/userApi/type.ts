3236020ff9e349d9779418baedb9223503af7861
Jonghyeon Ko <flowithusdev@gmail.com>
2022-01-28 18:20:00 +0900

    :recycle: refactor: UserApi 타이핑 (getUserData, getMyProfile)

--- src/service/userApi/type.ts
+++ src/service/userApi/type.ts
@@ -1,8 +1,17 @@
+import { APINotification } from "@domainTypes/tobe/notification";
+import { APIUser } from "@domainTypes/tobe/user";
 import { ApiPromise } from "@service/type";
 
 export interface UserApi {
-  getUserData: () => ApiPromise;
-  getMyProfile: () => ApiPromise;
+  getUserData: () => ApiPromise<{
+    user: APIUser;
+    notifications: APINotification[];
+  }>;
+  getMyProfile: () => ApiPromise<{
+    followerCount: number;
+    followingCount: number;
+    user: APIUser;
+  }>;
   getUserProfile: (...params: any[]) => ApiPromise;
   updateMyProfile: (...params: any[]) => ApiPromise;
   updateMyProfileImage: (...params: any[]) => ApiPromise;
