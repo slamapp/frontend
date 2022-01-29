a28dc7d33d704b322bdc1cbc1bb3ed12ba79eee4
Jonghyeon Ko <flowithusdev@gmail.com>
2022-01-30 02:58:55 +0900

    :recycle: refactor: 불필요한 기존 타입인 users 없애기

--- src/domainTypes/index.ts
+++ src/domainTypes/index.ts
@@ -1,18 +1,9 @@
-export { roles } from "./users";
 export { textures } from "./courts";
 export { status } from "./newCourts";
 
 export type { Follow } from "./follows";
 export type { Favorite } from "./favorites";
 export type { Reservation } from "./reservations";
-export type {
-  Role,
-  EditableUserProfile,
-  ProficiencyKeyUnion,
-  ProficiencyValueUnion,
-  PositionKeyUnion,
-  PositionValueUnion,
-} from "./users";
 export type { Coord } from "./map";
 export type { TextureValueUnion, TextureKeyUnion } from "./courts";
 export type { StatusValueUnion, StatusKeyUnion } from "./newCourts";
