23b820ceee48acebe7dbad2461ce976fd5bbce56
Jonghyeon Ko <flowithusdev@gmail.com>
2022-02-01 09:03:18 +0900

    ✨ feat: ChatroomListItem 추가

--- src/pages/chat/list/index.tsx
+++ src/pages/chat/list/index.tsx
@@ -1,13 +1,31 @@
-import { NextPage } from "next";
+import type { NextPage } from "next";
 import React from "react";
 import { useNavigationContext } from "@contexts/hooks";
 import UtilRoute from "UtilRoute";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
 const ChatroomListPage: NextPage = UtilRoute("private", () => {
   const { useMountPage } = useNavigationContext();
   useMountPage((page) => page.CHATROOM_LIST);
 
-  return <div>Chatroom List Page</div>;
+  return (
+    <div>
+      <ChatroomListItem>
+        <div>hi</div>
+      </ChatroomListItem>
+    </div>
+  );
 });
 
 export default ChatroomListPage;
+
+const ChatroomListItem = styled.div`
+  margin: 8px 16px;
+  padding: 16px;
+
+  ${({ theme }) => css`
+    border-radius: ${theme.borderRadiuses.lg};
+    background: ${theme.colors.white};
+  `}
+`;
