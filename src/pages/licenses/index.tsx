e4d11b53cc43101cd21a195dd8b35037f52e96ec
Jonghyeon Ko <flowithusdev@gmail.com>
2022-02-20 19:04:41 +0900

    ✨ feat: 오픈소스 라이센스 페이지 추가

---
+++ src/pages/licenses/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Licenses: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>오픈소스 라이센스 | Slam - 우리 주변 농구장을 빠르게</title>
+        <meta name="description" content="혼자서도 농구를 더 빠르게" />
+      </Head>
+    </div>
+  );
+};
+
+export default Licenses;
