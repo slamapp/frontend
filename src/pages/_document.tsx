f03c081be6648d562037a85073772d0ad5f05bda
마누 <manu.k@kakaocorp.com>
2022-06-28 21:40:26 +0900

    perf(Righteous, nomalize.css): 불필요한 script 제거 / 로고의 폰트 svg로 대체하기
    ISSUES CLOSED: #179

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,20 +25,6 @@ class MyDocument extends Document {
             type="text/css"
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
           />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://fonts.googleapis.com/css2?family=Righteous&display=swap"
-          />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-            integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
-            crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
-          />
           <script
             defer
             type="text/javascript"
