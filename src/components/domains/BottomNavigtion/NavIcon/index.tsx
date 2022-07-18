8acf6c6c191c34d6c6881591274597dc860e0b89
마누 <manu.k@kakaocorp.com>
2022-07-18 16:10:49 +0900

    fix(BottomNavigation/NavIcon): onTapStart에 route.push로 수정 항상 탭이 되도록 해결
    ISSUES CLOSED: #195

--- src/components/domains/BottomNavigtion/NavIcon/index.tsx
+++ src/components/domains/BottomNavigtion/NavIcon/index.tsx
@@ -1,10 +1,14 @@
 import type { ComponentProps } from "react"
+import { useCallback } from "react"
 import Link from "next/link"
+import { useRouter } from "next/router"
 import styled from "@emotion/styled"
 import { motion } from "framer-motion"
 import { Icon } from "~/components/uis/atoms"
 import { useNavigationContext } from "~/contexts/hooks"
 import type { PageType } from "~/contexts/NavigationProvider/actionTypes"
+
+const tap = { scale: 0.7 }
 
 interface Props {
   href: string
@@ -13,21 +17,22 @@ interface Props {
 }
 
 const NavIcon = ({ href, iconName, pageTypes }: Props) => {
+  const router = useRouter()
   const { navigationProps } = useNavigationContext()
   const { currentPage } = navigationProps
 
+  const handleClick = useCallback(() => router.push(href), [href, router])
+
   return (
-    <Link href={href} key={href} passHref>
-      <S.MotionAnchor whileTap={{ scale: 0.7 }}>
-        <Icon
-          name={iconName}
-          size={24}
-          color={
-            pageTypes.some((item) => item === currentPage) ? "black" : "#cfcfcf"
-          }
-        />
-      </S.MotionAnchor>
-    </Link>
+    <S.MotionAnchor onTapStart={handleClick} whileTap={tap}>
+      <Icon
+        name={iconName}
+        size={24}
+        color={
+          pageTypes.some((item) => item === currentPage) ? "black" : "#cfcfcf"
+        }
+      />
+    </S.MotionAnchor>
   )
 }
 
